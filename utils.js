5fb38f961aeaf0a807bc0b3eb7e0d787306fe4b4
Mike Kormendy <mike.kormendy@smartstartinc.com>
2019-10-23 21:48:51 -0500

    1. Remove default API key and related code 2. Add user-defined API key implementation 3. Add application-persistent plugin settings to save JSON and API keys 4. Minor UI cleanup and fix plugin name casing

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
-const uxp = require("uxp").storage;
-const apiKeys = require("./apiKeys.json").apiKeys;
-const fs = uxp.localFileSystem;
+const storage = require("uxp").storage;
+const fs = storage.localFileSystem;
+
+let data;
 
 /**
  * Downloads an image from the photoUrl and
@@ -12,7 +13,7 @@ async function downloadImage(photoUrl) {
     const photoObj = await xhrBinary(photoUrl);
     const tempFolder = await fs.getTemporaryFolder();
     const tempFile = await tempFolder.createFile("tmp", { overwrite: true });
-    await tempFile.write(photoObj, { format: uxp.formats.binary });
+    await tempFile.write(photoObj, { format: storage.formats.binary });
     return tempFile;
 }
 
@@ -45,14 +46,6 @@ function xhrBinary(url) {
         req.responseType = "arraybuffer";
         req.send();
     });
-}
-
-/**
- * Gets a random api key from the apikeys list in apikeys.json
- */
-function getApiKey() {
-    const length = apiKeys.length;
-    return apiKeys[Math.floor(Math.random()*length)];
 }
 
 /**
@@ -130,9 +123,88 @@ function getDimensions(node) {
     }
 }
 
+
+/**
+ * A little helper class to make storing key-value-pairs (e.g. settings) for plugins for Adobe XD CC easier.
+ */
+class storageHelper {
+    /**
+     * Creates a data file if none was previously existent.
+     * @return {Promise<storage.File>} The data file
+     * @private
+     */
+    static async init() {
+        let dataFolder = await fs.getDataFolder();
+        try {
+            let returnFile = await dataFolder.getEntry('storage.json');
+            data = JSON.parse((await returnFile.read({format: storage.formats.utf8})).toString());
+            return returnFile;
+        } catch (e) {
+            const file = await dataFolder.createEntry('storage.json', {type: storage.types.file, overwrite: true});
+            if (file.isFile) {
+                await file.write('{}', {append: false});
+                data = {};
+                return file;
+            } else {
+                throw new Error('Storage file storage.json was not a file.');
+            }
+        }
+    }
+
+    /**
+     * Retrieves a value from storage. Saves default value if none is set.
+     * @param {string} key The identifier
+     * @param {*} defaultValue The default value. Gets saved and returned if no value was previously set for the speciefied key.
+     * @return {Promise<*>} The value retrieved from storage. If none is saved, the `defaultValue` is returned.
+     */
+    static async get(key, defaultValue) {
+        if (!data) {
+            const dataFile = await this.init();
+            data = JSON.parse((await dataFile.read({format: storage.formats.utf8})).toString());
+        }
+        if (data[key] === undefined) {
+            await this.set(key, defaultValue);
+            return defaultValue;
+        } else {
+            return data[key];
+        }
+    }
+
+    /**
+     * Saves a certain key-value-pair to the storage.
+     * @param {string} key The identifier
+     * @param {*} value The value that get's saved
+     * @return {Promise<void>}
+     */
+    static async set(key, value) {
+        const dataFile = await this.init();
+        data[key] = value;
+        return await dataFile.write(JSON.stringify(data), {append: false, format: storage.formats.utf8})
+    }
+
+    /**
+     * Deletes a certain key-value-pair from the storage
+     * @param {string} key The key of the deleted pair
+     * @return {Promise<void>}
+     */
+    static async delete(key) {
+        return await this.set(key, undefined);
+    }
+
+    /**
+     * Resets (i.e. purges) all stored settings.
+     * @returns {Promise<void>}
+     */
+    static async reset() {
+        const dataFile = await this.init();
+        return await dataFile.write('{}', {append: false, format: storage.formats.utf8})
+
+    }
+}
+
 module.exports = {
     downloadImage,
-    getApiKey,
     parseStyles,
-    getDimensions
+    getDimensions,
+    storageHelper
 };
